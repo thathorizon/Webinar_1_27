c4739d8c47b2fb20959a62a16c2d9dc4f786c11b
Igor Avdonin <i.avdonin@okko.tv>
2024-11-19 19:52:54 +0300

    Внес изменения по результатам ревью

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Yandex!");
+        for (int i=0; i <= 99; i++) {
+            System.out.println("Привет, Яндекс Практикум!");
+        }
     }
 }
